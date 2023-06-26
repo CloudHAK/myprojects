b89ba27bbd889657999588134113aaf3a0c0fa5b
CloudHAK InfoTech <khumedsfdc2504@gmail.com>
2023-06-26 08:33:21 +0530

    initial Commit

---
+++ force-app/main/default/lwc/createaccountrecord/createaccountrecord.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Createaccountrecord extends LightningElement {}
--- force-app/main/default/lwc/lifecyclehookExample/lifecyclehookExample.js
+++ force-app/main/default/lwc/lifecyclehookExample/lifecyclehookExample.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class LifecyclehookExample extends LightningElement {}
+export default class LifecyclehookExample extends LightningElement {
+    @api message = 'Wellcome to CloudHAK';
+
+    constructor(){
+        super();
+        this.message = this.message + 'Life cycleHook';
+        console.log('Executing Consontructor');
+    }
+    connectedCallback(){
+        console.log('in connected Callback');
+    }
+    disconnectedCallback(){
+        console.log('in disconnected Callback');
+    }
+    renderedCallback(){
+        console.log('in rendered Callback');
+    }
+}
